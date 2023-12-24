c5c506699070b87389df75abd38eb471baaae6c0
Ahmed Abdelkarim <a.abdelkarim9696@gmail.com>
2023-12-24 14:33:22 +0200

    add rest of fields to the view

--- models/res_partner.py
+++ models/res_partner.py
@@ -1,8 +1,20 @@
 from odoo import models, fields
 
+
+CURRICULUM_TYPES = [
+    ('american', "American"),
+    ('british', "British"),
+]
+
 class ResPartner(models.Model):
     _inherit = 'res.partner'
     _description = 'Partner Inherit'
 
     manager_name = fields.Char(string="Manager Name")
     manager_phone = fields.Char(string="Manager Phone")
+    responsible_name = fields.Char(string="Responsible Name")
+    responsible_phone = fields.Char(string="Responsible Phone")
+    curriculum_type = fields.Selection(selection=CURRICULUM_TYPES, default='american')
+    
+    latitude = fields.Float()
+    longitude = fields.Float()
