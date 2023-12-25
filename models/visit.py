4bf6e7197d2d3ca83e49446f205763b9d73bf962
Ahmed Abdelkarim <a.abdelkarim9696@gmail.com>
2023-12-25 11:42:45 +0200

    create partner_visit relation and its views

--- models/visit.py
+++ models/visit.py
@@ -6,5 +6,6 @@ class Visit(models.Model):
     _description = 'School Visit'
     
     name = fields.Char('name')
+    partner_id = fields.Many2one(comodel_name='res.partner', string='Customer', required=True)
     notes = fields.Text('notes')
     date = fields.Datetime('date')
