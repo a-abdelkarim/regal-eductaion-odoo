cbac3cd53a0936ac989bb28467790d69ceb601e4
Ahmed Abdelkarim <a.abdelkarim9696@gmail.com>
2023-12-28 12:53:22 +0200

    replace city with state_id

--- models/visit.py
+++ models/visit.py
@@ -9,10 +9,11 @@ class Visit(models.Model):
     partner_id = fields.Many2one(comodel_name='res.partner', string='Customer', required=True)
     partner_name = fields.Char(related='partner_id.name')
     school_curriculum = fields.Selection(related='partner_id.curriculum_type')
-    school_city = fields.Char(related='partner_id.city')
+    school_state = fields.Many2one(related='partner_id.state_id')
     school_email = fields.Char(related='partner_id.email')
     notes = fields.Text('notes')
     date = fields.Datetime('date')
+    
     create_uid = fields.Many2one('res.users', 'Created By', default=lambda self: self.env.uid, readonly=True)
 
     @api.model
